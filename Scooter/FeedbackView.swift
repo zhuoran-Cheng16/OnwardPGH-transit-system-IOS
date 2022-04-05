413f735cbf6a452b3ef383e9f6c39b06da9231c4
然然 <52230092+zhuoran-cheng@users.noreply.github.com>
2022-04-05 17:38:47 -0400

    add button interface

--- Scooter/FeedbackView.swift
+++ Scooter/FeedbackView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct FeedbackView: View {
+    @State var button1="Happy"
+    @State var button2="Sad"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack {
+                Text("Please choose one")
+                    .foregroundColor(.blue)
+                    
+                Button(action:{
+                    print(button1)
+                }) {
+                    HStack {
+                        Image(systemName: "face.smiling")
+                        Text(button1)
+                    }.padding(10.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .stroke(lineWidth: 2.0)
+                        )
+                    }
+                Button(action:{
+                    print(button2)
+                }) {
+                    HStack {
+                        Image(systemName: "exclamationmark.circle.fill")
+                        Text(button2)
+                    }.padding(10.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .stroke(lineWidth: 2.0)
+                        )
+                    }
+                    
+                    Spacer()
+                }
+            }
+
+        }
 
 struct FeedbackView_Previews: PreviewProvider {
     static var previews: some View {
