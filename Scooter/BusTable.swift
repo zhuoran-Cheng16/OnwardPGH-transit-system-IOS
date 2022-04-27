f26169b9248afb527d680ad9e8701306424e7a6c
然然 <zcheng3@andrew.cmu.edu>
2022-04-27 02:58:15 -0400

    add bus api to get bus current location

--- Scooter/BusTable.swift
+++ Scooter/BusTable.swift
@@ -6,8 +6,64 @@
 //
 
 import SwiftUI
+import UIKit
+func matches(for regex: String, in text: String) -> [String] {
 
+    do {
+        let regex = try NSRegularExpression(pattern: regex)
+        let results = regex.matches(in: text,
+                                    range: NSRange(text.startIndex..., in: text))
+        return results.map {
+            String(text[Range($0.range, in: text)!])
+        }
+    } catch let error {
+        print("invalid regex: \(error.localizedDescription)")
+        return []
+    }
+}
+class BusGPS: ObservableObject {
+    @Published var attitude : String = ""
+    @Published var longtitude : String = ""
+    var dataset : String = ""
+    init(){
+        load()
+    }
+    func load() {
+        let url = URL(string: "https://truetime.portauthority.org/gtfsrt-bus/vehicles?debug")!
+        var urlRequest = URLRequest(url:url)
+        urlRequest.addValue("text/plain",forHTTPHeaderField: "Accept")
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+        if let data = data,
+        let strData = String(bytes: data, encoding: .utf8)
+                {
+
+            let components = strData.components(separatedBy: "entity")
+           
+            for k in components{
+                if k.contains("6650")
+                {
+                    let timestamp = k.components(separatedBy: "position {")
+             
+                    let weightt = timestamp[1].components(separatedBy: CharacterSet.init(charactersIn: "-0123456789.").inverted).joined(separator: "a")
+                    DispatchQueue.main.async { [self] in
+                    for i in weightt{
+                        if i != "a"{
+                            self.dataset.append(i)
+                            }
+                        }
+                        self.attitude=self.dataset.components(separatedBy: "-")[0]
+                        self.longtitude=self.dataset.components(separatedBy: "-")[1]
+
+                    }
+                }
+            }
+        } }.resume()
+
+    }
+
+}
 struct BusTable: View {
+    @ObservedObject var model:BusGPS=BusGPS()
     var body: some View {
         VStack(alignment: .leading) {
                        HStack {
@@ -22,6 +78,7 @@ struct BusTable: View {
                 Text("61A")
                             .font(.title)
                             .padding(.leading)
+
                        HStack {
                            Image(systemName: "figure.stand")
                                .padding(.trailing)
@@ -37,7 +94,6 @@ struct BusTable: View {
                                .foregroundColor(.gray)
                        }
                        .padding(.leading, 240.0)
-                       .frame(height: -10.0)
                        HStack {
                            Image (systemName: "arrow.forward.circle.fill")
                                .foregroundColor(.blue)
@@ -46,12 +102,22 @@ struct BusTable: View {
                                .font(.subheadline)
                                .foregroundColor(.blue)
                        }
+                
                        Text("Forbes Ave/Wightman")
                            .font(.subheadline)
                            .foregroundColor(.blue)
                            .padding(.leading)
                        Text("         Also in 37 min & 1h")
-                           .font(.subheadline)}
+                           .font(.subheadline)
+                HStack{
+                Text("Bus Current Location")
+                Text(model.attitude)
+                Text("-"+model.longtitude.prefix(8))
+                }.font(.footnote)
+                 .foregroundColor(.blue)
+                 .padding(.leading)
+        
+            }
                 Divider()
             VStack(alignment: .leading){
                 
